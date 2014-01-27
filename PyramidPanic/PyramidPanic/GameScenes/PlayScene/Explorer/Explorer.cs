2be01ac6ff96079fd9abc8eba2d6a96550ffb014
Mitchell <Mitchelltlam@hotmail.com>
2014-01-27 09:22:00 +0100

    Draaipunt verandert van de Draw method in AnimatedSprite class.

---
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/Explorer.cs
@@ -0,0 +1,107 @@
+﻿//met Using kan je een XNA codebibliotheer gebruiken in je class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic
+{
+    public class Explorer : IAnimatedSprite
+    {
+        //fields
+        private PyramidPanic game;
+        private IEntityState state;
+        private Texture2D texture;
+        private int speed = 3;
+        private Vector2 position;
+        private ExplorerIdle idle;
+
+        //Maak van iedere toestand (state) een field
+        private ExplorerWalkUp walkUp;
+        private ExplorerWalkDown walkDown;
+        private ExplorerWalkLeft walkLeft;
+        private ExplorerWalkRight walkRight;
+
+
+        //propperties
+        public IEntityState State
+        {
+            set { this.state = value; }
+        }
+        public ExplorerIdle Idle
+        {
+            get { return this.idle; }
+        }
+        public PyramidPanic Game
+        {
+            get { return this.game; }
+        }
+        public int Speed
+        {
+            get { return this.speed; }
+        }
+        public Texture2D Texture
+        {
+            get { return this.texture; }
+        }
+        public Vector2 Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+        public ExplorerWalkDown WalkDown
+        {
+            get { return this.walkDown; }
+        }
+        public ExplorerWalkUp WalkUp
+        {
+            get { return this.walkUp; }
+        }
+        public ExplorerWalkRight WalkRight
+        {
+            get { return this.walkRight; }
+        }
+        public ExplorerWalkLeft WalkLeft
+        {
+            get { return this.walkLeft; }
+        }
+
+
+
+        //constructor
+        public Explorer(PyramidPanic game, Vector2 position)
+        {
+            this.game = game;
+            this.position = position;
+            this.texture = game.Content.Load<Texture2D>(@"explorer/Explorer");
+            this.walkUp = new ExplorerWalkUp(this);
+            this.walkDown = new ExplorerWalkDown(this);
+            this.walkRight = new ExplorerWalkRight(this);
+            this.walkLeft = new ExplorerWalkLeft(this);
+            this.idle = new ExplorerIdle(this);
+            this.state = this.idle;
+
+        }
+
+
+
+        //update
+        public void Update(GameTime gameTime)
+        {
+            this.state.Update(gameTime);
+        }
+
+
+        //draw
+        public void Draw(GameTime gameTime)
+        {
+            this.state.Draw(gameTime);
+        }
+    }
+}
---
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/ExplorerWalkUp.cs
@@ -0,0 +1,71 @@
+﻿//met Using kan je een XNA codebibliotheer gebruiken in je class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic
+{
+    //Dit is een toestands class (dus moet hij de interface toepassen)
+    //Deze class belooft dan plechtig dat hij de methods uit de interface heeft
+    public class ExplorerWalkUp : AnimatedSprite, IEntityState
+    {
+        //fields
+        private Explorer explorer;
+        private Vector2 velocity;
+
+        //Constructor
+        public ExplorerWalkUp(Explorer explorer)
+            : base(explorer)
+        {
+            this.explorer = explorer;
+            this.destinationRectangle = new Rectangle((int)this.explorer.Position.X,
+                                                (int)this.explorer.Position.Y,
+                                                32,
+                                                32);
+            this.velocity = new Vector2(0f, this.explorer.Speed);
+
+        }
+
+        public void Initialize()
+        {
+            this.destinationRectangle.X = (int)this.explorer.Position.X;
+            this.destinationRectangle.Y = (int)this.explorer.Position.Y;
+        }
+
+        public new void Update(GameTime gameTime)
+        {
+            if (this.explorer.Position.X > 640 - 32)
+            {
+                //this.explorer.State = this.explorer.ExplorerWalkLeft;
+                //this.explorer.WalkRight.Initialize();
+            }
+
+            //Als de Rifht knop wordt losgelatenm dan moet de explorer weer in de toestand idle komen
+            if (Input.EdgeDetectKeyUp(Keys.Up))
+            {
+                this.explorer.State = this.explorer.Idle;
+                this.explorer.Idle.Initialize();
+                this.explorer.WalkDown.Initialize();
+                this.explorer.WalkLeft.Initialize();
+                this.explorer.WalkRight.Initialize();
+            }
+            this.explorer.Position -= this.velocity;
+            this.destinationRectangle.X = (int)this.explorer.Position.X;
+            this.destinationRectangle.Y = (int)this.explorer.Position.Y;
+            base.Update(gameTime);
+        }
+
+
+        public new void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
